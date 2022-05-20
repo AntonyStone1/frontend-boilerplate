3baa50a9332a841bd059ee62caecabe31fc496ee
Anton Kameniev <anton.kamenev69@gmail.com>
2022-05-21 02:36:27 +0300

    added tablet and mobile markups versions

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,21 @@
-console.log("Load script.js");
+try {
 
-// Instantiating the global app object
-console.log(123);
+  // toggle burger menu
+  const burgerMenu = document.querySelector('.nav_burger')
+  const navItems = document.querySelector('.nav_items')
+  const navItemsUl = document.querySelector('.nav_items ul')
+
+  const toggleBurger = () => {
+    navItems.classList.toggle('nav_items__active')
+  }
+
+  burgerMenu.addEventListener('click', toggleBurger)
+  document.addEventListener('click', (e) => {
+    if (e.target !== burgerMenu && e.target !== navItemsUl) {
+      navItems.classList.remove('nav_items__active')
+    }
+
+  })
+} catch (e) {
+  console.log(e);
+} 
